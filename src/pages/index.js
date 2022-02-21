9de999bb2d26e83d935184707816924bb24a9b54
Ashby McFall <ashbymcfall@hotmail.co.uk>
2022-02-21 21:40:44 +0900

    🎉 set up files, components, global styles

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import Acomplishments from "../components/Acomplishments/Acomplishments";
+import Hero from "../components/Hero/Hero";
+import Projects from "../components/Projects/Projects";
+import Technologies from "../components/Technologies/Technologies";
+import Timeline from "../components/TimeLine/TimeLine";
+import { Layout } from "../layout/Layout";
+
+const Home = () => {
+  return (
+    <Layout>
+      <Hero />
+      <Projects />
+      <Technologies />
+      <Timeline />
+      <Acomplishments />
+    </Layout>
+  );
+};
+
+export default Home;
