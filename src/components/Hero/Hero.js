f5069b040c484d7444cfc7bab5b558731c3dd245
Ashby McFall <ashbymcfall@hotmail.co.uk>
2022-02-22 13:58:38 +0900

    ✨ add hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,5 +1,32 @@
 import React from "react";
 
-const Hero = (props) => <div>Hero</div>;
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
+
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello I'm Ashby,
+        <br />a React Developer
+      </SectionTitle>
+      <SectionText>
+        A motivated and driven front end web developer specializing in React,
+        who is dedicated to creating responsive, user-friendly and aesthetically
+        pleasing web applications.
+      </SectionText>
+      <Button
+        onclick={() => (window.location = "https://github.com/ashby-git")}
+      >
+        Learn More
+      </Button>
+    </LeftSection>
+  </Section>
+);
 
 export default Hero;
