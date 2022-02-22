3430afb63ba4cab8c8f8ae82b1e763f1b156af17
Ashby McFall <ashbymcfall@hotmail.co.uk>
2022-02-22 19:20:41 +0900

    ✨ add projects section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,5 +1,43 @@
 import React from "react";
 
-const Projects = () => <div>Projects</div>;
+import * as S from "./ProjectsStyles";
+import {
+  Section,
+  SectionDivider,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import { projects } from "../../constants/constants";
+
+const Projects = () => (
+  <Section nopadding id="projects">
+    <SectionDivider />
+    <SectionTitle main>Projects</SectionTitle>
+    <S.GridContainer>
+      {projects.map((project) => (
+        <S.BlogCard key={project.id}>
+          <S.Img src={project.image} />
+          <S.TitleContent>
+            <S.HeaderThree title>{project.title}</S.HeaderThree>
+            <S.Hr />
+          </S.TitleContent>
+          <S.CardInfo>{project.description}</S.CardInfo>
+          <S.Hr />
+          <div>
+            <S.TitleContent>Stack</S.TitleContent>
+            <S.TagList>
+              {project.tags.map((tag, i) => (
+                <S.Tag key={i}>{tag}</S.Tag>
+              ))}
+            </S.TagList>
+          </div>
+          <S.UtilityList>
+            <S.ExternalLinks href={project.visit}>Live Site</S.ExternalLinks>
+            <S.ExternalLinks href={project.source}>Code</S.ExternalLinks>
+          </S.UtilityList>
+        </S.BlogCard>
+      ))}
+    </S.GridContainer>
+  </Section>
+);
 
 export default Projects;
---
+++ src/constants/constants.js
@@ -0,0 +1,48 @@
+export const projects = [
+  {
+    title: "DEARCOCO",
+    description:
+      "A static ReactJS website for a company manufacturing coconut skincare products.",
+    image: "/images/1.png",
+    tags: ["React", "React-Scroll", "AdobeXD"],
+    source: "https://github.com/ashby-git/dear-coco",
+    visit: "https://dear-coco.vercel.app/",
+    id: 0,
+  },
+  {
+    title: "GoCorona",
+    description:
+      "A ReactJS website to advertise a mobile healthcare advice application.",
+    image: "/images/2.png",
+    tags: ["React", "React-Router-Dom", "Styled Components", "Figma"],
+    source: "https://github.com/ashby-git/gocorona",
+    visit: "https://gocorona-am.vercel.app/",
+    id: 1,
+  },
+  {
+    title: "Chuck Norris Joke Generator",
+    description:
+      "This is a ReactJS web app where you can generate popular internet jokes about 80s action star Chuck Norris.",
+    image: "/images/3.png",
+    tags: ["React", "Rest API", "Styled Components"],
+    source: "https://github.com/ashby-git/chuck-norris-joke-generator",
+    visit: "https://chuck-norris-jokes-am.vercel.app/",
+    id: 2,
+  },
+  {
+    title: "MyMovieList",
+    description:
+      "A ReactJS website where you can sign up and track movies that you have watched and that you plan to watch on your own individual profile page.",
+    image: "/images/4.png",
+    tags: [
+      "React",
+      "Redux-Toolkit",
+      "React-Router-Dom",
+      "Rest API",
+      "Firebase & Firestore",
+    ],
+    source: "https://github.com/ashby-git/mymovielist",
+    visit: "https://mymovielist-am.vercel.app/",
+    id: 3,
+  },
+];
