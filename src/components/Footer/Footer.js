979a7fa4ed5500310bcacccd569d40d65733dfe9
Ashby McFall <ashbymcfall@hotmail.co.uk>
2022-02-25 14:31:11 +0900

    ✨ add footer section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
+
+import { SocialIcons } from "../Header/HeaderStyles";
+import * as S from "./FooterStyles";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <S.FooterWrapper>
+      <S.LinkList />
+      <S.SocialIconsContainer>
+        <S.CompanyContainer>
+          <S.Slogan>Ashby McFall</S.Slogan>
+        </S.CompanyContainer>
+        <S.SocialContainer>
+          <SocialIcons href="https://github.com/ashby-git">
+            <AiFillGithub size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="https://linkedin.com/in/ashby-mcfall-063627158">
+            <AiFillLinkedin size="3rem" />
+          </SocialIcons>
+        </S.SocialContainer>
+      </S.SocialIconsContainer>
+    </S.FooterWrapper>
+  );
 };
 
 export default Footer;
